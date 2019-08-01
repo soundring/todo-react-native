ce0ed5afcc65410e2073a8f58845527b4cd290cc
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2019-08-01 19:47:13 +0900

    Spanの説明を追加

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ここはJavaScript・HTMLでいうSpanみたいな書き方ができます</Text>
     </View>
   );
 }
