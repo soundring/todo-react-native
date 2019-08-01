f55a709def1f7bae638ea7c751d0c4e95713efb7
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2019-08-01 20:14:31 +0900

    Hooksを使ってStateを作成

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+// 次にやること
+// 文字入力の値を取得する
+// ボタンでその入力をアラート表示する
 
 export default function App() {
+   const [text, setText] = useState("なし")
+   const [todo,setTodo] = useState("無し")
   return (
     <View style={styles.container}>
       <TextInput
-        defaultValue="aaaaaaa"
-        style={{ width: 200, height: 44, padding: 8 }}
+        defaultValue={text}
+        onChangeText={(e) =>{
+            alert("入力された値",e,"Todoになるテキスト",text)
+        }}
+        style={{ width: 200, height: 44, padding: 8 ,backgroundColor: "red"}}
       />
-      <Text>JavaScript・HTMLでいうSpanみたいな書き方ができます</Text>
+      <Button
+      onPress={()=> {
+        setTodo("変更されたTODO")
+      }}
+      title="タスクを追加"
+      color="#841584"
+       />
+
+      <Text>{todo}</Text>
     </View>
   );
 }
