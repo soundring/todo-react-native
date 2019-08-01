908784852bc12364e3e3f5d3906d8023351fbec3
soundring <soundringing@yahoo.co.jp>
2019-08-01 20:04:52 +0900

    文字入力受付

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ここはJavaScript・HTMLでいうSpanみたいな書き方ができます</Text>
+      <TextInput
+        defaultValue="aaaaaaa"
+        style={{ width: 200, height: 44, padding: 8 }}
+      />
+      <Text>JavaScript・HTMLでいうSpanみたいな書き方ができます</Text>
     </View>
   );
 }
@@ -14,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
